417683f1049c2bed09aecb115a9c12a2fab79d75
joseph <jwpeter30@gmail.com>
2024-01-08 19:54:58 -0300

    feito

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,13 +2,20 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
+    // TODO: Implementar as propriedades faltantes de acordo com o diagrama
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string Modelo { get; set; }
+        protected string IMEI { get; set; }
+        protected int Tamanho { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int tamanho)
         {
+        // TODO: Passar os parâmetros do construtor para as propriedades
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo= modelo;
+            IMEI = imei;
+            Tamanho = tamanho;
+
         }
 
         public void Ligar()
